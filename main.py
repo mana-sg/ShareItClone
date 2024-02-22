b9e2583f01dd4323cc8e35609d2966c7d7dd47d0
manasg1111 <118659036+manasg1111@users.noreply.github.com>
2024-02-22 15:39:23 +0530

    Fixed import issues and bugs

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from .sender import Sender
-from .receiver import Receiver
+from sender import Sender
+from receiver import Receiver
 
 
 if __name__ == "__main__":
     print("Select 1 to send or 2 to receive file")
     choice = int(input())
     if choice == 1:
-        sender = Sender("user1") 
+        sender = Sender() 
         sender.receive_broadcast()
         sender.select_file()
         sender.send_file_names()
